2b11ab594c023d0c0c26ef08fe83add09512ff79
willjox <william.jox@gmail.com>
2019-12-07 15:48:35 +0100

    fully functional version.

--- arduinoClient/arduinoClient.ino
+++ arduinoClient/arduinoClient.ino
@@ -1,17 +1,18 @@
-//D #include <SparkFun_Si7021_Breakout_Library.h>
+//Uses a adafruit esp32 feather and Sparkfun si7021 temperature/humidty sensor
+#include <SparkFun_Si7021_Breakout_Library.h>
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <WiFi.h>
 
 // INTERNET
-const char* ssid = "oneonetwo fake";
-const char* password = "bluedragon";
+const char* ssid = "somessid";
+const char* password = "apassword";
 const char* mqtt_server = "farmer.cloudmqtt.com";
-#define mqtt_port 11850
-#define MQTT_USER "cbcsnhtj"
-#define MQTT_PASSWORD "V_5lMKhBkM-p"
-#define MQTT_SERIAL_PUBLISH_CH "/icircuit/ESP32/serialdata/tx"
-#define MQTT_SERIAL_RECEIVER_CH "/icircuit/ESP32/serialdata/rx"
+#define mqtt_port  11640
+#define MQTT_USER "jjxnaifl"
+#define MQTT_PASSWORD "bd5eXyqRYirm"
+#define MQTT_READINGS_CH "/test/tx"
+#define MQTT_MEASURE_FREQENCY_CH "/test/rx"
 #define LED 13
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -19,12 +20,11 @@ PubSubClient client(wifiClient);
 //Measurment
 float humidity = 0;
 float temp = 0;
-//D Weather sensor;
+Weather sensor;
 float measurmentFrq = 0.1;
 
 
 void setup() {
-  //debug
   Serial.begin(9600);
   Serial.println("starting setup");
   pinMode(LED, OUTPUT);
@@ -33,9 +33,10 @@ void setup() {
   setupMqtt();
   Serial.println("ending setup");
 }
+
 void setupSpark() {
   Serial.println("Starting setupSpark");
-//D   sensor.begin();
+  sensor.begin();
   Serial.println("ending setupSpark");
 }
 
@@ -46,70 +47,71 @@ void setupWifi() {
   delay(500);
   Serial.print(" . ");
  }
+ serial.println(" ")
  Serial.println("ending setupWifi");
 }
+//Handles recieved messages(poorly)
+void callback(char* topic, byte *payload, unsigned int length) {
+    Serial.println("-------new message from broker-----");
+    Serial.print("channel:");
+    Serial.println(topic);
+    Serial.print("data:");
+    char strPayload[8] = {0};
+    //Copies the payload byte by byte into the char array, will result in garbage if the bytes are not ascii code, Beware of overflow edge cases!
+    for (int i = 0; i < length && i < sizeof(strPayload) - 1; i++ ) {
+      strPayload[i] = (char) payload[i];
+    }
+    strcat(strPayload,"\0");
+    Serial.println(strPayload);
+    measurmentFrq = atof(strPayload);
+    //avoids negative frequency and divide by zero due to human error or garbage in payload
+    if (measurmentFrq <= 0) {
+      Serial.println("Error: invalid frequency");
+      measurmentFrq = 0.1;
+    }
+}
+
 
 void setupMqtt() {
   Serial.println("Starting setupMqtt");
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
   Serial.println("ending setupMqtt");
-
 }
 
 
 void loop() {
-  if(!client.loop()) {
-    client.reconnect();
-  }
+  client.loop();
   measure();
   Serial.print("temp: ");
   Serial.println(temp);
   Serial.print("Humidity: ");
   Serial.println(humidity);
   publishPayload();
-  digitalWrite(LED,HIGH);
-  delay(1/measurmentFrq * 10000);
-  digitalWrite(LED, LOW);
+  digitalWrite(LED,LOW);
+  delay(1/measurmentFrq * 1000);
+  digitalWrite(LED, HIGH);
 
 }
-
+//reads and stores humidty and temperature values.
 void measure() {
-//D   humidity = sensor.getRH();
-//D   temp = sensor.readTemp();
+    humidity = sensor.getRH();
+    temp = sensor.readTemp();
 }
-
-//MQTTSTUFF
-void publishPayload(char *data){
-  if (!client.connected()) {
-    reconnect();
-  }
-  //Sweet sweet overflow errors
-  char strTmp[32];
-  char strHum[32];
-  dtostrf(temp,5,2,strTmp);
-  dtostrf(humidity,5,2,strHum);
-  client.beginPublish(MQTT_SERIAL_PUBLISH_CH,70,false);
-  client.write(strtmp,32);
-  client.write(":",1);
-  client.write(strHum,32);
-  client.endPublish();
-}
-
+//reconnect() is copied from the pubsub example library
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-     Create a random client ID
-    String clientId = "ESP32Client-";
-    clientId += String(random(0xffff), HEX);
-     Attempt to connect
-    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
+     //Create a random client ID
+    char[] clientId = "labviewIot";
+     //Attempt to connect
+    if (client.connect(clientId,MQTT_USER,MQTT_PASSWORD)) {
       Serial.println("connected");
       //Once connected, publish an announcement...
-      client.publish("/icircuit/presence/ESP32/", "hello world");
+      client.publish("/hello/world", "hello world");
       // ... and resubscribe
-      client.subscribe(MQTT_SERIAL_RECEIVER_CH);
+      client.subscribe(MQTT_MEASURE_FREQENCY_CH);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -118,14 +120,21 @@ void reconnect() {
       delay(5000);
     }
   }
-
-void callback,(char* topic, byte *payload, unsigned int length) {
-    Serial.println("-------new message from broker-----");
-    Serial.print("channel:");
-    Serial.println(topic);
-    Serial.print("data:");
-    Serial.write(payload, length);
-    Serial.println();
-    //Very unsafe
-    //measurmentFrq = atof(payload)
+}
+//publishes readings to broker.
+void publishPayload(){
+  if (!client.connected()) {
+    reconnect();
+  }
+  //Sweet sweet overflow errors
+  char strTmp[16];
+  char strHum[16];
+  char strPayload[33] = {0};
+  dtostrf(temp,5,2,strTmp);
+  dtostrf(humidity,5,2,strHum);
+  strcat(strPayload,strTmp);
+  strcat(strPayload,":");
+  strcat(strPayload,strHum);
+  strcat(strPayload,"\0");
+  client.publish(MQTT_READINGS_CH,strPayload);
 }
